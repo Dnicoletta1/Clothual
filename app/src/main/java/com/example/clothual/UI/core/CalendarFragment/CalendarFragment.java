20f5a287f620524673188f6ec6a76fceed1c00a4
Davide Nicoletta <d.nicoletta1@campus.unimib.it>
2022-12-12 12:14:51 +0100

    Modifica Layout complessivo

--- app/src/main/java/com/example/clothual/UI/core/CalendarFragment/CalendarFragment.java
+++ app/src/main/java/com/example/clothual/UI/core/CalendarFragment/CalendarFragment.java
@@ -5,9 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.clothual.R;
+import com.example.clothual.databinding.FragmentCalendarBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +17,8 @@ import com.example.clothual.R;
  * create an instance of this fragment.
  */
 public class CalendarFragment extends Fragment {
+
+    private FragmentCalendarBinding binding;
 
     public CalendarFragment() {
     }
@@ -38,7 +42,16 @@ public class CalendarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        binding = FragmentCalendarBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
     }
 }
