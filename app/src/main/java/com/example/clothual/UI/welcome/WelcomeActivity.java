62c486d7ecf94aaebf3d316615c20933dcd042d8
Mattia Bertolotti <77412996+mattiabertolotti@users.noreply.github.com>
2023-01-05 10:23:53 +0100

    Disabilitato Back Button
    Disabilitato il "tasto indietro" per la WelcomeActivity in modo da non far vedere la SplashScreen quando si esce dall'app

--- app/src/main/java/com/example/clothual/UI/welcome/WelcomeActivity.java
+++ app/src/main/java/com/example/clothual/UI/welcome/WelcomeActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.clothual.R;
 
+import android.widget.Toast;
+
 public class WelcomeActivity extends AppCompatActivity {
 
 
@@ -14,5 +16,10 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_layout);
 
+    }
+    //disable backButton
+    @Override
+    public void onBackPressed(){
+        Toast.makeText(getApplicationContext(),"Premi il tasto home per uscire da Clothual", Toast.LENGTH_SHORT).show();
     }
 }
