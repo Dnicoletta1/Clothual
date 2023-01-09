241754c4503e88ddfc813f5644620c6ea5edc9ef
Mattia Bertolotti <77412996+mattiabertolotti@users.noreply.github.com>
2023-01-09 15:25:00 +0100

    Update WelcomeActivity.java
    Implementato anche Snackbar (che loro dicono di preferire) anche se io continuo a preferire Toast per uscire dall'app

--- app/src/main/java/com/example/clothual/UI/welcome/WelcomeActivity.java
+++ app/src/main/java/com/example/clothual/UI/welcome/WelcomeActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.clothual.R;
+import com.google.android.material.snackbar.Snackbar;
 
+import android.view.View;
 import android.widget.Toast;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -20,6 +22,11 @@ public class WelcomeActivity extends AppCompatActivity {
     //disable backButton
     @Override
     public void onBackPressed(){
-        Toast.makeText(getApplicationContext(),R.string.close_app, Toast.LENGTH_SHORT).show();
+        //Toast
+        //Toast.makeText(getApplicationContext(),R.string.close_app, Toast.LENGTH_SHORT).show();
+
+        //Snackbar
+        View view = findViewById(android.R.id.content);
+        Snackbar.make(view, R.string.close_app, Snackbar.LENGTH_SHORT).show();
     }
 }
